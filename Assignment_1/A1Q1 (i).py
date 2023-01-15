b8d6560c6597bcbfe8f6914f5a9ad7846228117b
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-15 19:09:33 +0530

    Add files via upload

---
+++ Assignment_1/A1Q1 (i).py
@@ -0,0 +1,48 @@
+Python 3.8.10 (default, Jun 22 2022, 20:18:18) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/student/Desktop/first.py ===================
+helllo world
+>>> 
+==================== RESTART: /home/student/Desktop/first.py ===================
+helllo world
+>>> 
+==================== RESTART: /home/student/Desktop/first.py ===================
+helllo world
+>>> 
+==================== RESTART: /home/student/Desktop/first.py ===================
+helllo world
+>>> 2+5
+7
+>>> a=10
+>>> a
+10
+>>> hello world
+SyntaxError: invalid syntax
+>>> print("hello world")
+hello world
+>>> 
+============== RESTART: /home/student/Desktop/2041011068/first.py ==============
+lab1
+what is your namepushpendra kumar
+my pushpendra kumar
+>>> 
+============== RESTART: /home/student/Desktop/2041011068/first.py ==============
+lab1
+what is your name
+============== RESTART: /home/student/Desktop/2041011068/first.py ==============
+lab1
+what is your name
+my name is  
+>>> pushpendra kumar
+SyntaxError: invalid syntax
+>>> 
+============== RESTART: /home/student/Desktop/2041011068/first.py ==============
+lab1
+what is your name2
+my name is  2
+>>> 
+======================== RESTART: /home/student/A1Q1.py ========================
+True
+>>> 
