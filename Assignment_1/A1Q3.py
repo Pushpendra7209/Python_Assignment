fe5ffd962d552a92fbad637310fc3f01f658fe82
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-15 19:10:48 +0530

    Add files via upload

---
+++ Assignment_1/A1Q3.py
@@ -0,0 +1,14 @@
+a='hi'>'hello'and 'bye'<'Bye'
+print(a)
+
+b='hi'>'hello' or 'bye'<'Bye'
+print(b)
+
+c=7>8or 5<6 and ' I am fine' >'I am not fine'
+print(c)
+
+d= 10!=9 and 29 >=29
+print(d)
+
+e= 10!=9 and 29 >=29 and 'hi' >'hello' or 'bye' <'Bye' and 7<2.5
+print(e)
