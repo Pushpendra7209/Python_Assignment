dbf466c1cba2f7b109114792934267a3953f9aa6
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-15 19:11:24 +0530

    Add files via upload

---
+++ Assignment_1/A1Q5.py
@@ -0,0 +1,29 @@
+a=15&22
+print(a)
+
+b=15|22
+print(b)
+
+c=-15 & 22
+print(c)
+
+d= -15 | 22
+print(d)
+
+e = ~15
+print(e)
+
+f = ~ 22
+print(f)
+
+g=~-22
+print(g)
+
+h= ~-20
+print(h)
+
+i= 8<<3
+print(i)
+
+j=40>>3
+print(j)
