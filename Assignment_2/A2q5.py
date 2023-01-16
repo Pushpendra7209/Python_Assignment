71723f6ddea0f6219e71e3e3c4da0863a62dfea1
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-17 01:30:08 +0530

    Add files via upload

---
+++ Assignment_2/A2q5.py
@@ -0,0 +1,15 @@
+#a
+print("      \t\t*\t\t\n")
+print("      \t*\t*\t*\t\n")
+print(" *\t*\t*\t*\t*\t\n")
+print("      \t*\t*\t*\t\n")
+print("      \t\t*\t\t\n")
+
+print()
+
+#b
+print("       $\t$\t$\t$\t$\n\n\n")
+print("       $\t\t\t\t$\n\n\n")
+print("       $\t\t\t\t$\n\n\n")
+print("       $\t\t\t\t$\n\n\n")
+print("       $\t$\t$\t$\t$\n\n\n")
