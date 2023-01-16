c015cc126b7e116a622425242ab491e4a3240e87
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-17 01:27:51 +0530

    Add files via upload

---
+++ Assignment_2/A2Q1.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.ceil(65.65))
+print(math.floor(65.47))
+print(math.fabs(67.58))
+print(math.fabs(3))
+print(math.exp(2.7))
+print(math.log(45.2))
+print(math.pow(4,1/2))
+print(math.sqrt(121))
+print(math.radians(30))
+print(math.degrees(math.pi/2))
+      
+
