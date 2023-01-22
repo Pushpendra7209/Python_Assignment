d8f9849d0e807b4cc31c0b40468b78e837884945
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-23 00:03:22 +0530

    Add files via upload

---
+++ Assignment_3/A3Q9.py
@@ -0,0 +1,8 @@
+aa = int(input("enter first number "))
+bb = int(input("enter second number "))
+def fun(a, b):
+    
+    c= int (a)+ int( b)
+   
+    return c
+print("sum of two number is : ",fun(aa,bb))
