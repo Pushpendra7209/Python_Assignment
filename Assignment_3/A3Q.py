30d6717471c0c61b514d756acefd95f3d079f05c
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-01-22 19:18:41 +0530

    Add files via upload

---
+++ Assignment_3/A3Q.py
@@ -0,0 +1,15 @@
+
+def fun(n):
+    sum=0
+    for i in range(1,n):
+        if n%i==0:
+            sum+=i
+
+            
+    if(sum==n):
+        print("perfect number")
+    else:
+        print("not perfect number")
+fun(15)     
+        
+        
